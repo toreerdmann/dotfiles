4cc20bceac2a43b7eaee2767ec429369a0e6e59d
Tore Erdmann <tore.erdmann@gmail.com>
2026-04-08 10:12:01 +0200

    add install script

---
+++ install_neovim.sh
@@ -0,0 +1,8 @@
+#!/bin/env bash
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim-linux-x86_64
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
+echo "export PATH=\"$PATH:/opt/nvim-linux-x86_64/bin\"" >>"$HOME/.bashrc"
+echo "export PATH=\"$PATH:/opt/nvim-linux-x86_64/bin\"" >>"$HOME/.zshrc"
