3d21096960294dc2912cbd53712236588e4369c3
Tore Erdmann <tore.erdmann@gmail.com>
2026-04-19 16:06:53 +0200

    sync light/dark themes

--- neovim/.config/nvim/lua/plugins/auto-dark-mode.lua
+++ neovim/.config/nvim/lua/plugins/auto-dark-mode.lua
@@ -7,20 +7,20 @@ return {
       vim.cmd("colorscheme catppuccin-mocha")
       -- Update git config for delta
       vim.fn.system("git config --global delta.features catppuccin-mocha")
-      -- Reload tmux if in a tmux session
-      if os.getenv("TMUX") then
-        vim.fn.system("tmux source-file ~/.tmux.conf")
-      end
+      -- Reload tmux
+      -- local home = os.getenv("HOME")
+      -- vim.fn.system("tmux set-option -g @catppuccin_flavor_override mocha")
+      -- vim.fn.system("tmux source-file " .. home .. "/.tmux.conf > /dev/null 2>&1 || true")
     end,
     set_light_mode = function()
       vim.api.nvim_set_option_value("background", "light", {})
       vim.cmd("colorscheme catppuccin-latte")
       -- Update git config for delta
       vim.fn.system("git config --global delta.features catppuccin-latte")
-      -- Reload tmux if in a tmux session
-      if os.getenv("TMUX") then
-        vim.fn.system("tmux source-file ~/.tmux.conf")
-      end
+      -- Reload tmux
+      local home = os.getenv("HOME")
+      -- vim.fn.system("tmux set-option -g @catppuccin_flavor_override latte")
+      -- vim.fn.system("tmux source-file " .. home .. "/.tmux.conf > /dev/null 2>&1 || true")
     end,
   },
 }
