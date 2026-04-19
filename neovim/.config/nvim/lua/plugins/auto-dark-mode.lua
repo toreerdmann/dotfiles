647fb6e5f6b69b8c21c7b11de3ac42bab1558d70
Tore Erdmann <tore.erdmann@gmail.com>
2026-04-19 15:44:41 +0200

    auto-dark-mode and git

--- neovim/.config/nvim/lua/plugins/auto-dark-mode.lua
+++ neovim/.config/nvim/lua/plugins/auto-dark-mode.lua
@@ -4,11 +4,23 @@ return {
     update_interval = 1000,
     set_dark_mode = function()
       vim.api.nvim_set_option_value("background", "dark", {})
-      vim.cmd("colorscheme tokyonight-moon")
+      vim.cmd("colorscheme catppuccin-mocha")
+      -- Update git config for delta
+      vim.fn.system("git config --global delta.features catppuccin-mocha")
+      -- Reload tmux if in a tmux session
+      if os.getenv("TMUX") then
+        vim.fn.system("tmux source-file ~/.tmux.conf")
+      end
     end,
     set_light_mode = function()
       vim.api.nvim_set_option_value("background", "light", {})
-      vim.cmd("colorscheme tokyonight-day")
+      vim.cmd("colorscheme catppuccin-latte")
+      -- Update git config for delta
+      vim.fn.system("git config --global delta.features catppuccin-latte")
+      -- Reload tmux if in a tmux session
+      if os.getenv("TMUX") then
+        vim.fn.system("tmux source-file ~/.tmux.conf")
+      end
     end,
   },
 }
