4441333ca506adc7a3f0cdd65c3c6bea717b706d
Tore Erdmann <tore.erdmann@gmail.com>
2026-07-16 10:12:40 +0200

    clipboard stuff

--- neovim/.config/nvim/lua/config/options.lua
+++ neovim/.config/nvim/lua/config/options.lua
@@ -4,4 +4,16 @@
 
 vim.g.lazyvim_python_lsp = "pyrefly"
 
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
+
 vim.opt.clipboard = "unnamedplus"
